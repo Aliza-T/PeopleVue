2d90eb8e4c3b9dea19f16fb72697859a0e2be5cb
Aliza Kushner <alizatkushner@gmail.com>
2018-12-13 14:25:12 -0500

    Added PeopleRepo and js

--- VuejsClass/Controllers/HomeController.cs
+++ VuejsClass/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using VuejsClass.Data;
 
 namespace VuejsClass.Controllers
 {
@@ -13,6 +14,34 @@ namespace VuejsClass.Controllers
             return View();
         }
 
-    
+        public ActionResult People()
+        {
+            return View();
+        }
+        [HttpPost]
+        public void Add(Person person)
+        {
+            var repo = new PeopleRepository(Properties.Settings.Default.ConStr);
+            repo.Add(person);
+        }
+        [HttpPost]
+        public void Edit(Person person)
+        {
+            var repo = new PeopleRepository(Properties.Settings.Default.ConStr);
+            repo.Update(person);
+        }
+        [HttpPost]
+        public void delete(int id)
+        {
+            var repo = new PeopleRepository(Properties.Settings.Default.ConStr);
+            repo.Delete(id);
+        }
+        public ActionResult Get()
+        {
+            var repo = new PeopleRepository(Properties.Settings.Default.ConStr);
+            return Json (repo.GetAll(), JsonRequestBehavior.AllowGet);
+        }
+
+
     }
 }
