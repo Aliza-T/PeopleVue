5da0dd372b28f1784f9211e9b23f3e914747a827
Aliza Kushner <alizatkushner@gmail.com>
2018-12-13 13:46:16 -0500

    Initial Commit

---
+++ VuejsClass/Scripts/Index.js
@@ -0,0 +1,18 @@
+﻿new Vue({
+    el: `#numberGenerated`,
+    data: {
+        number: '',
+        min: '',
+        max: '',
+    },
+    methods: {
+
+        buttonClick: function () {
+            this.number = getRandomArbitrary(+this.min, +this.max)
+        }
+    }
+})
+
+function getRandomArbitrary(min, max) {
+    return Math.random() * (max - min) + min;
+}
